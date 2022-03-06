693520c18b96e1d38359e5fd4692883a601e07fd
Yash Maheshwari <Yash.Maheshwari@ibm.com>
2022-03-06 12:32:49 +0530

    implemented rating functionality

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
+import { useState } from 'react';
 import {FaStar} from 'react-icons/fa';
 import './rating.css';
 function Rating() {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
     const stars = Array(5).fill(0);
-    console.log(stars);
+    
   return (
       <React.Fragment>
           <div className='rating'>
               {stars.map((star,index)=>{
                   return(
-                    <button><FaStar size={80}/></button>
+                    <button
+                    type='button'
+                    key={index}
+                    className={(hover || rating)>index ? "on":"off"}
+                    onMouseEnter = {()=>setHover(index+1)}
+                    onMouseLeave = {()=>setHover(rating)}
+                    onClick = {()=>setRating(index+1)}
+                    >
+                        <FaStar size={60}/>
+                        </button>
                   )
-              })}
+              }
+            )
+        }
+
           
           </div>
+          <h1>Your rating is : <span>{rating>0?rating:''}</span></h1>
       </React.Fragment>
   )
 }
